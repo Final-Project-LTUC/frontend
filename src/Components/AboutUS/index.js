e24038fb5b66ad2969ff9d38ffd14bba0912a7ca
NSALAA9 <alaansairat99@gmail.com>
2023-09-26 02:30:29 +0000

    starting

---
+++ src/Components/AboutUS/index.js
@@ -0,0 +1,358 @@
+// import React from 'react';
+// import {
+//   Box,
+//   chakra,
+//   Container,
+//   SimpleGrid,
+//   Stack,
+//   Text,
+//   VisuallyHidden,
+//   Input,
+//   IconButton,
+//   useColorModeValue,
+//   Flex,
+//   Center,
+//   HStack,
+// } from '@chakra-ui/react';
+// import { ReactNode } from 'react';
+// import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+// import { BiMailSend } from 'react-icons/bi';
+// import { IoLocationOutline, IoMailOutline, IoCallOutline } from 'react-icons/io5';
+
+// const Logo = (props) => {
+//   return (
+//     <svg height={32} viewBox="0 0 120 28" xmlns="http://www.w3.org/2000/svg" {...props}>
+//       {/* ... (SVG path data) */}
+//     </svg>
+//   );
+// };
+
+// const SocialButton = ({ children, label, href }) => {
+//   return (
+//     <chakra.button
+//       bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+//       rounded={'full'}
+//       w={8}
+//       h={8}
+//       cursor={'pointer'}
+//       as={'a'}
+//       href={href}
+//       display={'inline-flex'}
+//       alignItems={'center'}
+//       justifyContent={'center'}
+//       transition={'background 0.3s ease'}
+//       _hover={{
+//         bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+//       }}>
+//       <VisuallyHidden>{label}</VisuallyHidden>
+//       {children}
+//     </chakra.button>
+//   );
+// };
+
+// const ListHeader = ({ children }) => {
+//   return (
+//     <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+//       {children}
+//     </Text>
+//   );
+// };
+
+// const TeamMemberCard = ({ name, position, imageSrc, twitter, youtube, instagram }) => {
+//   return (
+//     <Box
+//       maxW={'270px'}
+//       w={'full'}
+//       bg={useColorModeValue('white', 'gray.800')}
+//       boxShadow={'2xl'}
+//       rounded={'lg'}
+//       p={6}
+//       textAlign={'center'}
+//     >
+//       <chakra.img
+//         w={32}
+//         h={32}
+//         mb={6}
+//         mx={'auto'}
+//         rounded={'full'}
+//         fallbackSrc='https://via.placeholder.com/150'
+//         src={imageSrc}
+//         alt={`${name}'s portrait`}
+//       />
+//       <Text fontWeight={600} fontSize={'xl'}>
+//         {name}
+//       </Text>
+//       <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')} mb={4}>
+//         {position}
+//       </Text>
+//       <Flex justifyContent={'center'}>
+//         <HStack spacing={3}>
+//           <SocialButton label={'Twitter'} href={twitter}>
+//             <FaTwitter />
+//           </SocialButton>
+//           <SocialButton label={'YouTube'} href={youtube}>
+//             <FaYoutube />
+//           </SocialButton>
+//           <SocialButton label={'Instagram'} href={instagram}>
+//             <FaInstagram />
+//           </SocialButton>
+//         </HStack>
+//       </Flex>
+//     </Box>
+//   );
+// };
+
+// const TeamMembers = () => {
+//   return (
+//     <Container as={Stack} maxW={'6xl'} py={10}>
+//       <Text fontWeight={'500'} fontSize={'xl'} mb={5}>
+//         Meet Our Team
+//       </Text>
+//       <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={4}>
+//         {/* Team Member 1 */}
+//         <TeamMemberCard
+//           name="John Doe"
+//           position="CEO"
+//           imageSrc="https://example.com/john.jpg"
+//           twitter="#"
+//           youtube="#"
+//           instagram="#"
+//         />
+//         {/* Team Member 2 */}
+//         <TeamMemberCard
+//           name="Jane Smith"
+//           position="CTO"
+//           imageSrc="https://example.com/jane.jpg"
+//           twitter="#"
+//           youtube="#"
+//           instagram="#"
+//         />
+//         {/* Team Member 3 */}
+//         <TeamMemberCard
+//           name="David Johnson"
+//           position="Designer"
+//           imageSrc="https://example.com/david.jpg"
+//           twitter="#"
+//           youtube="#"
+//           instagram="#"
+//         />
+//         {/* Team Member 4 */}
+//         <TeamMemberCard
+//           name="Sarah Davis"
+//           position="Developer"
+//           imageSrc="https://example.com/sarah.jpg"
+//           twitter="#"
+//           youtube="#"
+//           instagram="#"
+//         />
+//         {/* Team Member 5 */}
+//         <TeamMemberCard
+//           name="Michael Lee"
+//           position="Marketing"
+//           imageSrc="https://example.com/michael.jpg"
+//           twitter="#"
+//           youtube="#"
+//           instagram="#"
+//         />
+//       </SimpleGrid>
+//     </Container>
+//   );
+// };
+
+// export default function LargeWithNewsletter() {
+//   return (
+//     <Box
+//       bg={useColorModeValue('gray.50', 'gray.900')}
+//       color={useColorModeValue('gray.700', 'gray.200')}
+//     >
+//       <Container as={Stack} maxW={'6xl'} py={10}>
+//         {/* ... (Rest of your code) */}
+//         <TeamMembers /> {/* Add the TeamMembers component here */}
+       
+//       </Container>
+//     </Box>
+//   );
+// }
+
+import React from 'react';
+import {
+  Box,
+  chakra,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  VisuallyHidden,
+  Input,
+  IconButton,
+  useColorModeValue,
+  Flex,
+  Center,
+  HStack,
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { BiMailSend } from 'react-icons/bi';
+import { IoLocationOutline, IoMailOutline, IoCallOutline } from 'react-icons/io5';
+
+const Logo = (props) => {
+  return (
+    <svg height={32} viewBox="0 0 120 28" xmlns="http://www.w3.org/2000/svg" {...props}>
+      {/* ... (SVG path data) */}
+    </svg>
+  );
+};
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <chakra.button
+      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+      rounded={'full'}
+      w={8}
+      h={8}
+      cursor={'pointer'}
+      as={'a'}
+      href={href}
+      display={'inline-flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      transition={'background 0.3s ease'}
+      _hover={{
+        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+      }}
+    >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {children}
+    </chakra.button>
+  );
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+const TeamMemberCard = ({ name, position, imageSrc, twitter, youtube, instagram }) => {
+  return (
+    <Box
+      maxW={'270px'}
+      w={'full'}
+      bg={useColorModeValue('white', 'gray.800')}
+      boxShadow={'lg'}
+      rounded={'lg'}
+      p={6}
+      textAlign={'center'}
+      transition={'transform 0.2s ease, box-shadow 0.2s ease'}
+      _hover={{
+        transform: 'translateY(-8px)',
+        boxShadow: 'xl',
+      }}
+    >
+      <chakra.img
+        w={32}
+        h={32}
+        mb={6}
+        mx={'auto'}
+        rounded={'full'}
+        fallbackSrc='https://via.placeholder.com/150'
+        src={imageSrc}
+        alt={`${name}'s portrait`}
+      />
+      <Text fontWeight={600} fontSize={'xl'}>
+        {name}
+      </Text>
+      <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')} mb={4}>
+        {position}
+      </Text>
+      <Flex justifyContent={'center'}>
+        <HStack spacing={3}>
+          <SocialButton label={'Twitter'} href={twitter}>
+            <FaTwitter />
+          </SocialButton>
+          <SocialButton label={'YouTube'} href={youtube}>
+            <FaYoutube />
+          </SocialButton>
+          <SocialButton label={'Instagram'} href={instagram}>
+            <FaInstagram />
+          </SocialButton>
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+const TeamMembers = () => {
+  return (
+    <Container as={Stack} maxW={'6xl'} py={10}>
+      <Text fontWeight={'500'} fontSize={'xl'} mb={5}>
+        Meet Our Team
+      </Text>
+      <SimpleGrid columns={{ base: 1, md: 3, lg: 5 }} spacing={4}>
+        {/* Team Member 1 */}
+        <TeamMemberCard
+          name="John Doe"
+          position="CEO"
+          imageSrc="https://example.com/john.jpg"
+          twitter="#"
+          youtube="#"
+          instagram="#"
+        />
+        {/* Team Member 2 */}
+        <TeamMemberCard
+          name="Jane Smith"
+          position="CTO"
+          imageSrc="https://example.com/jane.jpg"
+          twitter="#"
+          youtube="#"
+          instagram="#"
+        />
+        {/* Team Member 3 */}
+        <TeamMemberCard
+          name="David Johnson"
+          position="Designer"
+          imageSrc="https://example.com/david.jpg"
+          twitter="#"
+          youtube="#"
+          instagram="#"
+        />
+        {/* Team Member 4 */}
+        <TeamMemberCard
+          name="Sarah Davis"
+          position="Developer"
+          imageSrc="https://example.com/sarah.jpg"
+          twitter="#"
+          youtube="#"
+          instagram="#"
+        />
+        {/* Team Member 5 */}
+        <TeamMemberCard
+          name="Michael Lee"
+          position="Marketing"
+          imageSrc="https://example.com/michael.jpg"
+          twitter="#"
+          youtube="#"
+          instagram="#"
+        />
+      </SimpleGrid>
+    </Container>
+  );
+};
+
+export default function LargeWithNewsletter() {
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}
+    >
+      <Container as={Stack} maxW={'6xl'} py={10}>
+        {/* ... (Rest of your code) */}
+        <TeamMembers /> {/* Add the TeamMembers component here */}
+     
+       
+      </Container>
+    </Box>
+  );
+}
