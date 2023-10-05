26eb5e695c320abaa1cf54bb4e5984efe46d98c0
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-10-05 19:14:38 +0000

    hero edit

--- src/pages/home/Hero.js
+++ src/pages/home/Hero.js
@@ -1,5 +1,4 @@
 import {
-  AspectRatio,
   Button,
   Flex,
   Stack,
@@ -10,30 +9,55 @@ import {
 import React from "react";
 import { Link as ChakraLink } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+const cards = [
+  {
+    id: 1,
+    image:
+      "https://irp-cdn.multiscreensite.com/d64336ae/dms3rep/multi/desktop/handyman-2100x900.jpg",
+  },
+  {
+    id: 2,
+    image: "https://www.corralejo.info/images/Ferramenta-Portada2.jpg",
+  },
+  {
+    id: 3,
+    image:
+      "https://th.bing.com/th/id/R.f7dcad3108819cd6b39bc6dbb53737a0?rik=veRlPhjbRuu75g&riu=http%3a%2f%2fded7t1cra1lh5.cloudfront.net%2fsite_banners%2f65253%2fcrop%2fhandyman_hero.jpg%3f1573857999&ehk=%2be8zD6Zajw0yRVf8DjgP5Gn%2fQmgtDMwc8DHuh%2bbHAsQ%3d&risl=&pid=ImgRaw&r=0",
+  },
+  {
+    id: 4,
+    image:
+      "https://mlqjqxveabkl.i.optimole.com/leaubAo-oTKc4k2P/w:1600/h:1068/q:mauto/f:avif/https://stroisbyt.kz/wp-content/uploads/2022/05/mt-1828-content-bg-2.jpg",
+  },
+];
+
+console.log(cards);
 
 function Hero() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const updateImageIndex = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === cards.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(updateImageIndex, 4000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div>
       <Flex
         w={"100%"}
-        h={"80vh"}
+        h={"70vh"}
         mx={"auto"}
-        backgroundImage={
-          
-          "url(https://irp-cdn.multiscreensite.com/d64336ae/dms3rep/multi/desktop/handyman-2100x900.jpg)"
-        }
+        backgroundImage={`url(${cards[currentImageIndex].image})`}
         backgroundSize={"cover"}
         backgroundPosition={"center center"}
-        This video will have equal sides
       >
-{/* <AspectRatio  mx={"auto"} ratio={1}  backgroundSize={"cover"}
-        backgroundPosition={"center center"}>
-<iframe
-  title='naruto'
-  src='https://www.youtube.com/embed/FlwtxQQG-JU?si=2gAH0wDWlj78klYM' 
-  allowFullScreen
-/>
-</AspectRatio> */}
         <VStack
           w={"full"}
           justify={"center"}
