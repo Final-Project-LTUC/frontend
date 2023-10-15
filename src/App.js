160ae6eaf6cb5c455739fa5d90acca2496f8bfe5
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-10-15 12:53:11 +0000

    almost

--- src/App.js
+++ src/App.js
@@ -1,46 +1,38 @@
 import "./App.css";
 
-import React from 'react';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-import Navbar from "./layout/header/Navbar/Navbar";
+import Navbar from "../src/pages/home/homeHero/Navbar";
 import Services from "./pages/services/Services";
 import Catalog from "./pages/catalog/Catalog";
-import AboutUs from './pages/aboutUs/ABOUTUS';
+import AboutUs from "./pages/aboutUs/ABOUTUS";
 
 import Footer from "../src/Components/AboutUS/NavBar/Footer";
 
 import Dashboard from "./pages/dashBoard/Dashboard";
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import UpdateForm from "./pages/dashBoard/update/Update";
 import HandymanDetails from "./pages/catalog/HandymanDetails";
-import DashboardNavbar from './Components/dashboard/DashboardNavbar'; 
+import DashboardNavbar from "./Components/dashboard/DashboardNavbar";
 
-
-
-import Tasks from  "./pages/dashBoard/tasks/TasksPage"
+import Tasks from "./pages/dashBoard/tasks/TasksPage";
 
 import Auth from "./pages/auth/Auth";
 import LoginProvider from "./hooks/Context/LoginProvider";
 import ServicesTest from "./pages/services/ServicesTest/ServicesTest";
 
-
-
-
-
 // Create and export the socket connection
 
 function App() {
-  const location = useLocation(); 
-
+  const location = useLocation();
 
   // Define a function to conditionally render the navbar based on the current route
   const renderNavbar = () => {
     if (location.pathname === "/dashboard" || location.pathname === "/tasks") {
       return null;
     } else if (location.pathname === "/") {
-      
     } else {
       return <Navbar />;
     }
@@ -49,30 +41,24 @@ function App() {
   return (
     <div className="App">
       <LoginProvider>
-      {renderNavbar()}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/services" element={<Services />} /> */}
-        <Route path="/services" element={<ServicesTest  hue="220" />} />
-
-         <Route path="/catalog" element={<Catalog />} />
-                <Route
-                    path="/handyman/:id"
-                    element={<HandymanDetails />}
-                />{" "}
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/update" element={<UpdateForm />} />        
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path='/signup' element={<Auth submitAction='signup' />}/>
-        <Route path='/signin' element={<Auth submitAction='signin' />}/>
-      </Routes>
-
+        {renderNavbar()}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/services" element={<Services />} /> */}
+          <Route path="/services" element={<ServicesTest hue="220" />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/handyman/:id" element={<HandymanDetails />} />{" "}
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/update" element={<UpdateForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/signup" element={<Auth submitAction="signup" />} />
+          <Route path="/signin" element={<Auth submitAction="signin" />} />
+        </Routes>
       </LoginProvider>
       <Footer />
     </div>
   );
-
 }
 
 export default App;
--- src/pages/home/homeHero/Navbar.js
+++ src/pages/home/homeHero/Navbar.js
@@ -1,5 +1,4 @@
-
-import React from 'react'
+import React from "react";
 import { NavLink, Link as ReactRouterLink } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
@@ -24,82 +23,102 @@ import {
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { useContext } from "react";
-import {LoginContext} from '../../../hooks/Context/LoginProvider'
+import { LoginContext } from "../../../hooks/Context/LoginProvider";
 const Links = ["Skilify", "Home", "Services", "Catalog", "About us"];
 
 function Navbar() {
+  const [isNavFixed, setIsNavFixed] = React.useState(false);
 
-    const [isNavFixed, setIsNavFixed] = React.useState(false);
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
 
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-  
-      if (scrollPosition > 0 && !isNavFixed) {
-        setIsNavFixed(true);
-      } else if (scrollPosition === 0 && isNavFixed) {
-        setIsNavFixed(false);
-      }
+    if (scrollPosition > 0 && !isNavFixed) {
+      setIsNavFixed(true);
+    } else if (scrollPosition === 0 && isNavFixed) {
+      setIsNavFixed(false);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
-  
-    React.useEffect(() => {
-      window.addEventListener("scroll", handleScroll);
-  
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, [isNavFixed]);
-  
-    const { colorMode, toggleColorMode } = useColorMode();
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const {loginData,logout}=useContext(LoginContext);
-    console.log(loginData)
-    const mar = 80;
+  }, [isNavFixed]);
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { loginData, logout } = useContext(LoginContext);
+  console.log(loginData);
+  const mar = 80;
+
   return (
-    <div>
-           <Flex
-                h={16}
-                alignItems={"center"}
+    <header>
+      <div className="heroSection">
+        <div className="container">
+          <nav
+            style={{
+              marginLeft: mar,
+              position: isNavFixed ? "fixed" : "relative",
+              top: 0,
+              left: 0,
+              width: isNavFixed ? "100%" : "auto",
+              height: "auto",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: isNavFixed ? "rgba(254, 251, 251, 0.362)" : "transparent",
+              boxShadow: isNavFixed ? "0 15px 15px rgba(0, 0, 0, 0.3)" : "none",
+              borderBottom: isNavFixed ? "1px solid rgba(255, 255, 255, 0.1)" : "none",
+              borderTop: isNavFixed ? "1px solid rgba(255, 255, 255, 0.1)" : "none",
+              borderRadius: isNavFixed ? "30px" : "none",
+              padding: isNavFixed ? "10px" : "none",
+              letterSpacing: isNavFixed ? "1px" : "none",
+              textDecoration: "none",
+              overflow: "hidden",
+              color: isNavFixed ? "black" : "auto",
+              fontWeight: isNavFixed ? "400" : "auto",
+              zIndex: isNavFixed ? "1000" : "auto",
+              transition: "0.5s",
+              backdropFilter: isNavFixed ? "blur(15px)" : "none",
+              overflowY: "hidden", // This prevents vertical scrollbar
+            }}
+          >
+            <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+              <IconButton
+                size={"md"}
+                icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                aria-label={"Open Menu"}
+                display={{ md: "none" }}
+                onClick={isOpen ? onClose : onOpen}
+              />
+
+              <HStack
+                // as={"nav"}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
                 justifyContent={"space-between"}
               >
-                <IconButton
-                  size={"md"}
-                  icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                  aria-label={"Open Menu"}
-                  display={{ md: "none" }}
-                  onClick={isOpen ? onClose : onOpen}
-                />
-
-                <HStack
-                  // as={"nav"}
-                  spacing={4}
-                  display={{ base: "none", md: "flex" }}
-                  justifyContent={"space-between"}
-                >
-                  <div className="logo"> 
-
+                <div className="logo">
                   <ChakraLink as={ReactRouterLink} to="/">
                     <img src={logo} alt="Logo" />
                   </ChakraLink>
-                  </div>
-                  <ChakraLink as={ReactRouterLink} to="/">
-                    Home
-                  </ChakraLink>
-                  <ChakraLink as={ReactRouterLink} to="/services">
-                    Services
-                  </ChakraLink>
-                  <ChakraLink as={ReactRouterLink} to="/catalog">
-                    Catalog
-                  </ChakraLink>
-                  <ChakraLink as={ReactRouterLink} to="/about">
-                    About
-                  </ChakraLink>
-
-                
-                  
-             
+                </div>
+                <ChakraLink as={ReactRouterLink} to="/">
+                  Home
+                </ChakraLink>
+                <ChakraLink as={ReactRouterLink} to="/services">
+                  Services
+                </ChakraLink>
+                <ChakraLink as={ReactRouterLink} to="/catalog">
+                  Catalog
+                </ChakraLink>
+                <ChakraLink as={ReactRouterLink} to="/about">
+                  About
+                </ChakraLink>
 
                 <Flex alignItems={"center"}>
-            
                   <Stack
                     flex={{ base: 1, md: 0 }}
                     justify={"flex-end"}
@@ -110,95 +129,97 @@ function Navbar() {
                   >
                     <Button onClick={toggleColorMode}>
                       {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                    </Button> 
-                     {loginData.loggedIn? 
-
-                    <Menu>
-                      <MenuButton
-                        as={Button}
-                        rounded={"full"}
-                        variant={"link"}
-                        cursor={"pointer"}
-                        minW={0}
-                      >
-                        <Avatar
-                          size={"sm"}
-                          src={
-                            "https://avatars.dicebear.com/api/male/username.svg"
-                          }
-                        />
-                      </MenuButton>
-                      <MenuList alignItems={"center"}>
-                        <br />
-                        <Center>
+                    </Button>
+                    {loginData.loggedIn ? (
+                      <Menu>
+                        <MenuButton
+                          as={Button}
+                          rounded={"full"}
+                          variant={"link"}
+                          cursor={"pointer"}
+                          minW={0}
+                        >
                           <Avatar
-                            size={"2xl"}
+                            size={"sm"}
                             src={
                               "https://avatars.dicebear.com/api/male/username.svg"
                             }
                           />
-                        </Center>
-                        <br />
-                        <Center>
-                          <p>Username</p>
-                        </Center>
-                        <br />
-                        <MenuDivider />
-                        <ChakraLink as={ReactRouterLink} to="/dashboard">
-                          <MenuItem>Your Dashboard</MenuItem>
-                        </ChakraLink>
-                        
-                        <MenuItem>Account Settings</MenuItem>
+                        </MenuButton>
+                        <MenuList alignItems={"center"}>
+                          <br />
+                          <Center>
+                            <Avatar
+                              size={"2xl"}
+                              src={
+                                "https://avatars.dicebear.com/api/male/username.svg"
+                              }
+                            />
+                          </Center>
+                          <br />
+                          <Center>
+                            <p>Username</p>
+                          </Center>
+                          <br />
+                          <MenuDivider />
+                          <ChakraLink as={ReactRouterLink} to="/dashboard">
+                            <MenuItem>Your Dashboard</MenuItem>
+                          </ChakraLink>
 
-                        <ChakraLink as={ReactRouterLink} onClick={()=>logout()} to="/">
-                        <MenuItem>Logout</MenuItem>
-                        </ChakraLink>
+                          <MenuItem>Account Settings</MenuItem>
 
-                      </MenuList>
-                    </Menu>
-                   :null} 
+                          <ChakraLink
+                            as={ReactRouterLink}
+                            onClick={() => logout()}
+                            to="/"
+                          >
+                            <MenuItem>Logout</MenuItem>
+                          </ChakraLink>
+                        </MenuList>
+                      </Menu>
+                    ) : null}
 
-
-
-                    {loginData.loggedIn? 
-                    <></>
-              //  <Button onClick={()=>logout()}>Logout</Button>
-               :
-             <>
-              <Button
-                as={"a"}
-                fontSize={"sm"}
-                fontWeight={400}
-                variant={"link"}
-                href={"/signin"}
-                onClick={onOpen}
-                >
-                Sign In
-              </Button>
-              <Button
-                as={"a"}
-                display={{ base: "none", md: "inline-flex" }}
-                fontSize={"sm"}
-                fontWeight={600}
-                color={"white"}
-                bg={"teal.400"}
-                href={"/signup"}
-                _hover={{
-                  bg: "teal.300",
-                }}
-                onClick={onOpen}
-                >
-                Sign Up
-              </Button>
-              </> 
-             }
+                    {loginData.loggedIn ? (
+                      <></>
+                    ) : (
+                      //  <Button onClick={()=>logout()}>Logout</Button>
+                      <>
+                        <Button
+                          as={"a"}
+                          fontSize={"sm"}
+                          fontWeight={400}
+                          variant={"link"}
+                          href={"/signin"}
+                          onClick={onOpen}
+                        >
+                          Sign In
+                        </Button>
+                        <Button
+                          as={"a"}
+                          display={{ base: "none", md: "inline-flex" }}
+                          fontSize={"sm"}
+                          fontWeight={600}
+                          color={"white"}
+                          bg={"teal.400"}
+                          href={"/signup"}
+                          _hover={{
+                            bg: "teal.300",
+                          }}
+                          onClick={onOpen}
+                        >
+                          Sign Up
+                        </Button>
+                      </>
+                    )}
                   </Stack>
                 </Flex>
-                
               </HStack>
-              </Flex>
-    </div>
-  )
+            </Flex>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
