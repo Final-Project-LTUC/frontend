98ee138bc5802cac43275a2c0a98be32bb2d3ef2
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-10-16 22:12:23 +0000

    bellandinquiryDateevent

--- src/Components/icons/BellComponent.jsx
+++ src/Components/icons/BellComponent.jsx
@@ -1,10 +1,33 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import { Box, Flex, Icon } from '@chakra-ui/react';
 import { FaBell } from 'react-icons/fa';
 import gsap from 'gsap';
 import Notifications from '../notifications/Notifications';
+import { LoginContext } from "../../hooks/Context/LoginProvider";
 
 const BellComponent = () => {
+  const { socket } = useContext(LoginContext);
+
+  const [notificationCount, setNotificationCount] = useState(0);
+  const [payload, setPayload] = useState('');
+
+  useEffect(() => {
+    const handleInquiryDate = (payload) => {
+      console.log("Notification received. Payload:", payload);
+      setPayload(payload)
+      setNotificationCount((prevCount) => {
+        console.log("Previous count:", prevCount);
+        return prevCount + 1;
+      });
+    };
+
+    socket.on("inquiryDate", handleInquiryDate);
+
+    // return () => {
+    //   socket.off("inquiryDate", handleInquiryDate);
+    // };
+  }, [socket]); 
+
   const bellRef = useRef(null);
   const circleRef = useRef(null);
   const [showNotification, setNotification] = useState(false);
@@ -30,10 +53,11 @@ const BellComponent = () => {
       repeat: 5,
       yoyo: true,
     });
-  }, []);
+  }, [showNotification]);
 
   const toggleNotification = () => {
     setNotification(!showNotification);
+    setNotificationCount(0);
   };
 
   return (
@@ -50,23 +74,23 @@ const BellComponent = () => {
             backgroundColor: 'lightgray',
           }}
           onClick={toggleNotification}
-          position="relative" // Added relative positioning
+          position="relative" 
         >
           <Flex className="container__content" direction="column" align="center">
             <Box className="bell" mb={4} ref={bellRef}>
-              <Box className="notification">nooo</Box>
+              <Box className="notification">{notificationCount}</Box>
               <Icon as={FaBell} className="bell-icon" boxSize={6} />
               <Box className="bell-icon__circle" ref={circleRef}></Box>
             </Box>
           </Flex>
           {showNotification && (
             <Box
-              position="absolute" // Position the Notifications component absolutely
-              top="100%" // Adjust the top value to your desired position
-              right={0} // Adjust the right value to your desired position
-              zIndex={1} // Ensure Notifications appear above the bell icon
+              position="absolute" 
+              top="100%" 
+              right={0} 
+              zIndex={1} 
             >
-              <Notifications />
+              <Notifications showNotification={showNotification} payload={payload} socket={socket}/>
             </Box>
           )}
         </Box>
--- src/Components/notifications/Notifications.js
+++ src/Components/notifications/Notifications.js
@@ -1,109 +1,107 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./notifications.css";
-
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 
-function Notifications() {
+function Notifications({ showNotification, payload,socket }) {
+  const [inquiryDatePayload, setInquiryDatePayload] = useState([]);
   useEffect(() => {
-    (function () {
-      let optionBtns = document.querySelectorAll(".js-option");
+    if (showNotification && payload) {
+      setInquiryDatePayload((prevPayloads) => {
+        console.log(prevPayloads, "ggggggggggggggggggg");
+        if (!prevPayloads) {
+          return payload;
+        } else {
+          const updatedPayloads = [...prevPayloads, payload];
+          console.log(updatedPayloads, "hahahahahahaahah");
+          return updatedPayloads;
+        }
+      });
+    }
+  }, [payload,showNotification]);
 
-      for (var i = 0; i < optionBtns.length; i++) {
-        optionBtns[i].addEventListener("click", function (e) {
-          var notificationCard = this.parentNode.parentNode;
-          var clickBtn = this;
+  const handleOptionClick = (option, payload) => {
+    if (option === "archive") {
+      socket.emit("transaction", payload);
+    } else if (option === "delete") {
+      socket.emit("serviceRejected", payload);
+    }
+  };
 
-          requestAnimationFrame(function () {
-            archiveOrDelete(clickBtn, notificationCard);
+  useEffect(() => {
+    let optionBtns = document.querySelectorAll(".js-option");
+
+    for (var i = 0; i < optionBtns.length; i++) {
+      optionBtns[i].addEventListener("click", function (e) {
+        var notificationCard = this.parentNode.parentNode;
+        var clickBtn = this;
+
+        requestAnimationFrame(function () {
+          archiveOrDelete(clickBtn, notificationCard);
+
+          window.setTimeout(function () {
+            requestAnimationFrame(function () {
+              notificationCard.style.transition = "all .4s ease";
+              notificationCard.style.height = 0;
+              notificationCard.style.margin = 0;
+              notificationCard.style.padding = 0;
+            });
 
             window.setTimeout(function () {
-              requestAnimationFrame(function () {
-                notificationCard.style.transition = "all .4s ease";
-                notificationCard.style.height = 0;
-                notificationCard.style.margin = 0;
-                notificationCard.style.padding = 0;
-              });
-
-              window.setTimeout(function () {
-                notificationCard.parentNode.removeChild(notificationCard);
-              }, 1500);
+              notificationCard.parentNode.removeChild(notificationCard);
             }, 1500);
-          });
+          }, 1500);
         });
-      }
+      });
+    }
 
-      var archiveOrDelete = function (clickBtn, notificationCard) {
-        if (clickBtn.classList.contains("archive")) {
-          notificationCard.classList.add("archive");
-        } else if (clickBtn.classList.contains("delete")) {
-          notificationCard.classList.add("delete");
-        }
-      };
-    })();
-  }, []);
+    var archiveOrDelete = function (clickBtn, notificationCard) {
+      if (clickBtn.classList.contains("archive")) {
+        notificationCard.classList.add("archive");
+        handleOptionClick("archive", payload);
+      } else if (clickBtn.classList.contains("delete")) {
+        notificationCard.classList.add("delete");
+        handleOptionClick("delete", payload);
+      }
+    };
+  }, [showNotification, payload, handleOptionClick, socket]);
 
   return (
     <div className="wrapper">
       <div className="wrapper">
         <div className="notifications">
-          <div className="notifications__item">
-            <div className="notifications__item__avatar">
-              <img
-                src="https://github.com/Flat-Pixels/Notifications-card-animation/raw/master/img/avatar_1.jpg"
-                alt=""
-              />
-            </div>
-
-            <div className="notifications__item__content">
-              <span className="notifications__item__title">Verdieu Steeve</span>
-              <span className="notifications__item__message">
-                Just started following you
-              </span>
-            </div>
-
-            <div>
-              <div className="notifications__item__option archive js-option">
-                <i className="fas fa-folder">
-                  {" "}
-                  <CheckIcon />
-                </i>
+          {inquiryDatePayload.map((payload, index) => (
+            <div key={index} className="notifications__item">
+              <div className="notifications__item__avatar">
+                <img
+                  src={payload.avatarSrc}
+                  alt={`Avatar of ${payload.title}`}
+                />
               </div>
-              <div className="notifications__item__option delete js-option">
-                <i className="fas fa-trash">
-                  <CloseIcon />
-                </i>
-              </div>
-            </div>
-          </div>
-          <div className="notifications__item">
-            <div className="notifications__item__avatar">
-              <img
-                src="https://github.com/Flat-Pixels/Notifications-card-animation/raw/master/img/avatar_1.jpg"
-                alt=""
-              />
-            </div>
 
-            <div className="notifications__item__content">
-              <span className="notifications__item__title">Verdieu Steeve</span>
-              <span className="notifications__item__message">
-                Just started following you
-              </span>
-            </div>
-
-            <div>
-              <div className="notifications__item__option archive js-option">
-                <i className="fas fa-folder">
-                  {" "}
-                  <CheckIcon />
-                </i>
+              <div className="notifications__item__content">
+                <span className="notifications__item__title">
+                  {payload.title}
+                </span>
+                <span className="notifications__item__message">
+                  appointment: {payload.schdualedAt}
+                </span>
               </div>
-              <div className="notifications__item__option delete js-option">
-                <i className="fas fa-trash">
-                  <CloseIcon />
-                </i>
+
+              <div>
+                <div className="notifications__item__option archive js-option">
+                  <i className="fas fa-folder">
+                    {" "}
+                    <CheckIcon />
+                  </i>
+                </div>
+                <div className="notifications__item__option delete js-option">
+                  <i className="fas fa-trash">
+                    <CloseIcon />
+                  </i>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
