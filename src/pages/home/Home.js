dc82e823fea5150a6a6116bc04c318c74a191467
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-10-16 15:10:50 +0000

    Notifications

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,12 +5,14 @@ import HomeCatalog from "./HomeCatalog";
 import HomeBlog from "./bolg/HomeBlog";
 import HowWeWork from "../home/howWeWork/HowWeWork";
 import Hero from "./homeHero/Hero";
+// import Notifications from "../../Components/notifications/Notifications";
 
 export default function Home() {
   return (
     <div className="home-container">
       <Hero />
       <TrustUs />
+      {/* <Notifications/> */}
       <HowWeWork />
       <HomeServices />
       <HomeCatalog />
