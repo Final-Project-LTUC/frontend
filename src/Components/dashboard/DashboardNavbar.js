26192eb402b6e02139173bfd88957576607d06a1
Armada6199 <moh.abdin27@gmail.com>
2023-10-21 18:28:54 +0300

    profile

--- src/Components/dashboard/DashboardNavbar.js
+++ src/Components/dashboard/DashboardNavbar.js
@@ -2,6 +2,9 @@
 import { useState } from "react"; // Import useState
 import { Link as ReactRouterLink } from "react-router-dom";
 import { Link } from "react-router-dom";
+import logo from '../../assets/logo-a-BqCGck--transformed-removebg-preview.png';
+import { FaChartBar, FaHome, FaUser, FaMoneyBillWave, FaEdit, FaTasks } from "react-icons/fa";
+import './profile.css'
 import {
   Box,
   Flex,
@@ -49,12 +52,12 @@ export default function Simple({
   return (
     <>
       <Box
-        bg={"teal"}
         h={"100vh"}
         position={"sticky"}
-        top={"0px"}
+        top={"10px"}
         color={"white"}
-        w={"20%"}
+        w={"15%"}
+        className="glassNav"
       >
         <Flex
           h={"100%"}
@@ -78,42 +81,55 @@ export default function Simple({
             justifyContent={"space-around"}
             alignItems={"center"}
             h={"80%"}
-            gap={"5%"}
+            _hover={
+              {}
+            }
           >
-            <Image src={profilePicUrl} w={"48"} borderRadius={"10px"} />
+            <Image src={logo} w={"64"} borderRadius={"10px"} />
+            <Flex w={'100%'} h={'12%'} justifyContent={'space-between'} flexDirection={'column'}  alignItems={'center'}>
+            <FaHome fontSize={'28'}/>
             <Link
               style={{ fontSize: "1.3em" }}
               textDecoration={"none"}
               as={ReactRouterLink}
               to="/"
-            >
+              >
               Home
             </Link>
+              </Flex>
+              <Flex w={'100%'} h={'12%'}  justifyContent={'space-between'} flexDirection={'column'}  alignItems={'center'}>
+              <FaUser fontSize={'28'}/>
             <Link
               style={{ fontSize: "1.3em" }}
               onClick={()=>setShowNum(0) }
-            >
+              >
               Profile
             </Link>
+              </Flex>
+              <Flex w={'100%'} h={'12%'} justifyContent={'space-between'} flexDirection={'column'} alignItems={'center'}>
+                <FaEdit fontSize={'28'}/>
             <Link
               style={{ fontSize: "1.3em" }} 
               onClick={()=>setShowNum(1)}
-            >
-              Update data
+              >
+              Update
             </Link>
+              </Flex>
+              <Flex w={'100%'} h={'12%'} alignItems={'center'} flexDirection={'column'}   justifyContent={'space-between'}>
+                <FaTasks fontSize={'28'}/>
             <Link style={{ fontSize: "1.3em" }} 
            onClick={()=>setShowNum(2)}>
               Tasks
             </Link>
-            {token.role==='handyman'&&
+            </Flex>
+            {token.role==='handyman'&&<Flex h={'12%'} flexDirection={'column'}  w={'100%'} jjustifyContent={'space-between'} alignItems={'center'}>
+            <FaChartBar fontSize={'28'}/>
             <Link
             onClick={()=>setShowNum(3)}
             style={{ fontSize: "1.3em" }}>
               Earnings
-            </Link>}
-            <Button w={"32"} onClick={toggleColorMode}>
-              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-            </Button>
+            </Link>
+              </Flex>}
           </HStack>
         </Flex>
 
@@ -130,3 +146,126 @@ export default function Simple({
     </>
   );
 }
+
+
+
+
+// import { useState } from "react";
+// import {
+//   Box,
+//   Flex,
+
+//   HStack,
+
+//   IconButton,
+//   Button,
+//   useColorMode,
+// } from "@chakra-ui/react";
+// import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+// import { FaChartBar, FaHome, FaUser, FaMoneyBillWave, FaEdit, FaTasks } from "react-icons/fa";
+// import { Link, Link as ReactRouterLink } from "react-router-dom";
+
+// const SidebarItems = [
+//   { label: "Dashboard", icon: <FaChartBar size={20} />, path: "/dashboard" },
+//   { label: "Home", icon: <FaHome size={20}/>, path: "/" },
+//   { label: "Profile", icon: <FaUser size={20}/>, path: "/profile" },
+//   { label: "Earnings", icon: <FaMoneyBillWave size={20}/>, path: "/earnings" },
+//   { label: "Update Data", icon: <FaEdit size={20}/>, path: "/update-data" },
+//   { label: "Tasks", icon: <FaTasks size={20}/>, path: "/tasks" },
+// ];
+
+
+// export default function Sidebar({
+//   profilePicUrl,
+//   setShowUpdateForm,
+//   setShowTasks,
+//   token,
+//   setShowNum
+// }) {
+
+//   const { colorMode, toggleColorMode } = useColorMode();
+//   const [isDashboardSidebarOpen, setDashboardSidebarOpen] = useState(false);
+
+//   const toggleSidebar = () => {
+//     setDashboardSidebarOpen(!isDashboardSidebarOpen);
+//   };
+
+//   return (
+//     <Box
+//     bg="teal.500"
+//     h="100vh"
+//     position="sticky"
+//     top="0"
+//     color="white"
+//     w={isDashboardSidebarOpen ? "160px" : "70px"}
+//     transition="width 0.2s"
+//   >
+//     <Flex h="100%" flexDirection="column" alignItems="center">
+//       <IconButton
+//         size="lg"
+//         icon={isDashboardSidebarOpen ? <CloseIcon /> : <HamburgerIcon />}
+//         aria-label="Toggle Menu"
+//         onClick={toggleSidebar}
+//       />
+  
+//       <Flex as="nav" w="100%" flexDirection="column" justifyContent="space-between" alignItems="center" h="80%" gap={1}>
+//         {SidebarItems.map((item, index) => (
+//           <Button
+//             as={ReactRouterLink}
+//             to={item.path}
+//             w="100%"
+//             variant="ghost"
+//             color="white"
+//             leftIcon={item.icon}
+//             justifyContent="start"
+//             key={index}
+//             _hover={{ bg: "teal.600" }}
+//             p={7}
+//             display="flex"
+//           >
+//             {isDashboardSidebarOpen ? item.label : null}
+//             {item.path === "/" && "Home"}
+  
+//             {item.path === "/profile" && (
+//               <Link style={{ fontSize: "1.3em" }} onClick={() => setShowNum(0)}>
+//                 Profile
+//               </Link>
+//             )}
+  
+//             {item.path === "/update-data" && (
+//               <Link style={{ fontSize: "1.3em" }} onClick={() => setShowNum(1)}>
+//                 Update data
+//               </Link>
+//             )}
+  
+//             {item.path === "/tasks" && (
+//               <Link style={{ fontSize: "1.3em" }} onClick={() => setShowNum(2)}>
+//                 Tasks
+//               </Link>
+//             )}
+  
+//             {token.role === "handyman" && item.path === "/earnings" && (
+//               <Link style={{ fontSize: "1.3em" }} onClick={() => setShowNum(3)}>
+//                 Earnings
+//               </Link>
+//             )}
+//           </Button>
+//         ))}
+//         <Button w="32" onClick={toggleColorMode}>
+//           {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+//         </Button>
+//       </Flex>
+  
+//       <IconButton
+//         w="50px"
+//         h="50px"
+//         icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+//         aria-label="Toggle Dark Mode"
+//         onClick={toggleColorMode}
+//       />
+//     </Flex>
+//   </Box>
+  
+//   );
+// }
+
